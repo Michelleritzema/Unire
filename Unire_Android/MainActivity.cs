643cd68da4d9384299d448c6d10a220d9e9b0482
Michelleritzema <michelleritzema@hotmail.com>
2015-04-24 22:23:19 +0200

    Notifications

--- Unire_Android/MainActivity.cs
+++ Unire_Android/MainActivity.cs
@@ -6,26 +6,56 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace Unire_Android
-{
+namespace Unire_Android {
     [Activity(Label = "Unire_Android", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
-        int count = 1;
+    
+	public class MainActivity : Activity {
 
-        protected override void OnCreate(Bundle bundle)
-        {
+        protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            Button purple_notification = FindViewById<Button>(Resource.Id.Purple);
+            purple_notification.Click += delegate { notification("Purple", "Notification: purple", "noti_purple"); };
+            Button blue_notification = FindViewById<Button>(Resource.Id.Blue);
+            blue_notification.Click += delegate { notification("Blue", "Notification: blue", "noti_blue"); };
+            Button green_notification = FindViewById<Button>(Resource.Id.Green);
+            green_notification.Click += delegate { notification("Green", "Notification: green", "noti_green"); };
+            Button yellow_notification = FindViewById<Button>(Resource.Id.Yellow);
+            yellow_notification.Click += delegate { notification("Yellow", "Notification: yellow", "noti_yellow"); };
+            Button orange_notification = FindViewById<Button>(Resource.Id.Orange);
+            orange_notification.Click += delegate { notification("Orange", "Notification: orange", "noti_orange"); };
+            Button red_notification = FindViewById<Button>(Resource.Id.Red);
+            red_notification.Click += delegate { notification("Red", "Notification: red", "noti_red"); };
         }
+
+		public void notification(String title, String text, String icon) {
+			// Instantiate the builder and set notification elements:
+            Notification.Builder builder = new Notification.Builder(this)
+                .SetContentTitle(title)
+                .SetContentText(text)
+                .SetDefaults(NotificationDefaults.Sound | NotificationDefaults.Vibrate);
+				
+            switch() {
+                case Purple:
+
+            }
+
+                builder.SetSmallIcon(Resource.Drawable.noti_purple);
+                
+                
+
+			// Build the notification:
+			Notification notification = builder.Build();
+
+			// Get the notification manager:
+			NotificationManager notificationManager =
+				GetSystemService(Context.NotificationService) as NotificationManager;
+
+			// Publish the notification:
+			const int notificationId = 0;
+			notificationManager.Notify(notificationId, notification);
+		}
     }
 }
 
--- Unire_Android/obj/Release/android/unire_android/unire_android/R.java
+++ Unire_Android/obj/Release/android/unire_android/unire_android/R.java
@@ -13,16 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int monoandroidsplash=0x7f020001;
+        public static final int noti_purple=0x7f020002;
     }
     public static final class id {
-        public static final int MyButton=0x7f060000;
+        public static final int Blue=0x7f060001;
+        public static final int Green=0x7f060002;
+        public static final int Orange=0x7f060004;
+        public static final int Purple=0x7f060000;
+        public static final int Red=0x7f060005;
+        public static final int Yellow=0x7f060003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int ApplicationName=0x7f040001;
-        public static final int Hello=0x7f040000;
+        public static final int ApplicationName=0x7f040000;
+        public static final int Blue=0x7f040005;
+        public static final int Green=0x7f040004;
+        public static final int Orange=0x7f040006;
+        public static final int Purple=0x7f040001;
+        public static final int Red=0x7f040002;
+        public static final int Yellow=0x7f040003;
     }
     public static final class style {
         public static final int Mono_Android_Theme_Splash=0x7f050000;

bb7c1be9f0c300c6b68f4ee4753ae55902242b3e
Michelleritzema <michelleritzema@hotmail.com>
2015-04-24 11:54:06 +0200

    DLL Shizzle

--- Unire_Android/obj/Debug/android/unire/unire/R.java
+++ Unire_Android/obj/Debug/android/unire/unire/R.java
@@ -12,9 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int monoandroidsplash=0x7f020001;
     }
     public static final class id {
-        public static final int MyButton=0x7f050000;
+        public static final int MyButton=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -22,5 +23,8 @@ public final class R {
     public static final class string {
         public static final int ApplicationName=0x7f040001;
         public static final int Hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int Mono_Android_Theme_Splash=0x7f050000;
     }
 }
